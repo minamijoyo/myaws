96bad73b52dad6ff770dc6fdae2e6e5528762dce
Masayuki Morita <minamijoyo@gmail.com>
2016-08-21 18:49:22 +0900

    Extract ec2ls implementaion to lib/myaws/ec2

--- cmd/ec2ls.go
+++ cmd/ec2ls.go
@@ -1,12 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/minamijoyo/myaws/lib/myaws/ec2"
 	"github.com/spf13/cobra"
 )
 
@@ -15,62 +10,9 @@ var ec2lsCmd = &cobra.Command{
 	Use:   "ls",
 	Short: "List EC2 instances",
 	Long:  `List EC2 instances`,
-	Run:   ec2ls,
+	Run:   ec2.Ls,
 }
 
 func init() {
 	ec2Cmd.AddCommand(ec2lsCmd)
-}
-
-func ec2ls(cmd *cobra.Command, args []string) {
-	svc := ec2.New(
-		session.New(),
-		&aws.Config{
-			Region: aws.String("ap-northeast-1"),
-		},
-	)
-
-	params := &ec2.DescribeInstancesInput{
-		Filters: []*ec2.Filter{
-			{
-				Name: aws.String("instance-state-name"),
-				Values: []*string{
-					aws.String("running"),
-				},
-			},
-		},
-	}
-
-	resp, err := svc.DescribeInstances(params)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, res := range resp.Reservations {
-		for _, inst := range res.Instances {
-			fmt.Println(formatEC2Instance(inst))
-		}
-	}
-}
-
-func formatEC2Instance(inst *ec2.Instance) string {
-	output := []string{
-		*inst.PublicIpAddress,
-		*inst.InstanceId,
-		*inst.State.Name,
-		(*inst.LaunchTime).Format("2006-01-02 15:04:05"),
-		lookupEC2Tag(inst, "Name"),
-	}
-	return strings.Join(output[:], "\t")
-}
-
-func lookupEC2Tag(inst *ec2.Instance, key string) string {
-	var value string
-	for _, t := range inst.Tags {
-		if *t.Key == key {
-			value = *t.Value
-			break
-		}
-	}
-	return value
 }
---
+++ lib/myaws/ec2/ls.go
@@ -0,0 +1,64 @@
+package ec2
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/spf13/cobra"
+)
+
+func Ls(*cobra.Command, []string) {
+	svc := ec2.New(
+		session.New(),
+		&aws.Config{
+			Region: aws.String("ap-northeast-1"),
+		},
+	)
+
+	params := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name: aws.String("instance-state-name"),
+				Values: []*string{
+					aws.String("running"),
+				},
+			},
+		},
+	}
+
+	resp, err := svc.DescribeInstances(params)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, res := range resp.Reservations {
+		for _, inst := range res.Instances {
+			fmt.Println(formatInstance(inst))
+		}
+	}
+}
+
+func formatInstance(inst *ec2.Instance) string {
+	output := []string{
+		*inst.PublicIpAddress,
+		*inst.InstanceId,
+		*inst.State.Name,
+		(*inst.LaunchTime).Format("2006-01-02 15:04:05"),
+		lookupTag(inst, "Name"),
+	}
+	return strings.Join(output[:], "\t")
+}
+
+func lookupTag(inst *ec2.Instance, key string) string {
+	var value string
+	for _, t := range inst.Tags {
+		if *t.Key == key {
+			value = *t.Value
+			break
+		}
+	}
+	return value
+}
