f7471b9918da13545d9120f6d3ee4bc29c902db7
Masayuki Morita <minamijoyo@gmail.com>
2016-09-07 22:39:34 +0900

    Rename local variables in ec2 ls

--- myaws/ec2/ls.go
+++ myaws/ec2/ls.go
@@ -19,14 +19,14 @@ func Ls(*cobra.Command, []string) {
 		},
 	}
 
-	resp, err := client.DescribeInstances(params)
+	response, err := client.DescribeInstances(params)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, res := range resp.Reservations {
-		for _, inst := range res.Instances {
-			fmt.Println(formatInstance(inst, viper.GetString("ec2.ls.output-tags")))
+	for _, reservation := range response.Reservations {
+		for _, instance := range reservation.Instances {
+			fmt.Println(formatInstance(instance, viper.GetString("ec2.ls.output-tags")))
 		}
 	}
 }
@@ -58,41 +58,41 @@ func buildTagFilter(filterTag string) *ec2.Filter {
 	return tagFilter
 }
 
-func formatInstance(inst *ec2.Instance, outputTags string) string {
+func formatInstance(instance *ec2.Instance, outputTags string) string {
 	output := []string{
-		*inst.InstanceId,
-		*inst.InstanceType,
-		publicIpAddress(inst),
-		*inst.PrivateIpAddress,
-		*inst.State.Name,
-		(*inst.LaunchTime).Format("2006-01-02 15:04:05"),
+		*instance.InstanceId,
+		*instance.InstanceType,
+		publicIpAddress(instance),
+		*instance.PrivateIpAddress,
+		*instance.State.Name,
+		(*instance.LaunchTime).Format("2006-01-02 15:04:05"),
 	}
-	tags := lookupTags(inst, outputTags)
+	tags := lookupTags(instance, outputTags)
 	output = append(output, tags...)
 	return strings.Join(output[:], "\t")
 }
 
-func publicIpAddress(inst *ec2.Instance) string {
+func publicIpAddress(instance *ec2.Instance) string {
 	ip := "___.___.___.___"
-	if *inst.State.Name == "running" {
-		ip = *inst.PublicIpAddress
+	if *instance.State.Name == "running" {
+		ip = *instance.PublicIpAddress
 	}
 	return ip
 }
 
-func lookupTags(inst *ec2.Instance, keys string) []string {
+func lookupTags(instance *ec2.Instance, keys string) []string {
 	tags := strings.Split(keys, ",")
 	var values []string
 
 	for _, tag := range tags {
-		values = append(values, lookupTag(inst, tag))
+		values = append(values, lookupTag(instance, tag))
 	}
 	return values
 }
 
-func lookupTag(inst *ec2.Instance, key string) string {
+func lookupTag(instance *ec2.Instance, key string) string {
 	var value string
-	for _, t := range inst.Tags {
+	for _, t := range instance.Tags {
 		if *t.Key == key {
 			value = *t.Value
 			break
