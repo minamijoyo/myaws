3669906a03153894ace6e1bfaf99df926be9298b
Masayuki Morita <minamijoyo@gmail.com>
2016-09-08 14:51:02 +0900

    Print LoadBalancerNames and InstanceIds in autoscaling ls

--- myaws/autoscaling/ls.go
+++ myaws/autoscaling/ls.go
@@ -2,7 +2,10 @@ package autoscaling
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/spf13/cobra"
 )
@@ -17,4 +20,40 @@ func Ls(*cobra.Command, []string) {
 	}
 
 	fmt.Println(response)
+	for _, asg := range response.AutoScalingGroups {
+		fmt.Println(formatAutoScalingGroup(asg))
+	}
+}
+
+func formatAutoScalingGroup(asg *autoscaling.Group) string {
+	var instanceLength int
+	if asg.Instances != nil {
+		instanceLength = len(asg.Instances)
+	}
+	output := []string{
+		strconv.Itoa(instanceLength),
+		*asg.AutoScalingGroupName,
+	}
+
+	if asg.LoadBalancerNames != nil {
+		loadBalancerNames := aws.StringValueSlice(asg.LoadBalancerNames)
+		output = append(output, loadBalancerNames...)
+	} else {
+		output = append(output, "")
+	}
+
+	var instanceIds []string
+	if instanceLength > 0 {
+		instanceIds = lookupInstanceIds(asg.Instances)
+		output = append(output, instanceIds...)
+	}
+	return strings.Join(output[:], "\t")
+}
+
+func lookupInstanceIds(instances []*autoscaling.Instance) []string {
+	var instanceIds []string
+	for _, instance := range instances {
+		instanceIds = append(instanceIds, *instance.InstanceId)
+	}
+	return instanceIds
 }
