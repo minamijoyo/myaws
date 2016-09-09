3f8c33de2c8d21f6c559ed00bfe34f856c5f97fa
Masayuki Morita <minamijoyo@gmail.com>
2016-09-09 17:58:27 +0900

    Add autoscaling update command

---
+++ myaws/autoscaling/update.go
@@ -0,0 +1,32 @@
+package autoscaling
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func Update(cmd *cobra.Command, args []string) {
+	client := newAutoScalingClient()
+
+	asgName := aws.String(args[0])
+	desiredCapacity := viper.GetInt64("autoscaling.update.desired-capacity")
+	if desiredCapacity == -1 {
+		panic("desired-capacity must be set.")
+	}
+
+	params := &autoscaling.SetDesiredCapacityInput{
+		AutoScalingGroupName: asgName,
+		DesiredCapacity:      &desiredCapacity,
+	}
+
+	response, err := client.SetDesiredCapacity(params)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response)
+}
