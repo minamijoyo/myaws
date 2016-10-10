b0857eb2c02e539b1813b531247822cb0a08d089
Masayuki Morita <minamijoyo@gmail.com>
2016-10-10 22:23:11 +0900

    Add support IAM Role credentials

--- myaws/config.go
+++ myaws/config.go
@@ -1,10 +1,15 @@
 package myaws
 
 import (
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/spf13/viper"
 )
 
@@ -16,11 +21,19 @@ func NewConfig() *aws.Config {
 }
 
 func newCredentials(profile string) *credentials.Credentials {
-	if profile != "" {
-		return credentials.NewSharedCredentials("", profile)
-	} else {
-		return credentials.NewEnvCredentials()
-	}
+	return credentials.NewChainCredentials(
+		[]credentials.Provider{
+			&credentials.SharedCredentialsProvider{
+				Profile: profile,
+			},
+			&credentials.EnvProvider{},
+			&ec2rolecreds.EC2RoleProvider{
+				Client: ec2metadata.New(session.New(&aws.Config{
+					HTTPClient: &http.Client{Timeout: 3000 * time.Millisecond},
+				},
+				)),
+			},
+		})
 }
 
 func getRegion(region string) *string {
