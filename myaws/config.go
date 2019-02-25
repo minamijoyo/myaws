a970ac3e01b042be64da7e3f44710741ef8920be
Masayuki Morita <minamijoyo@gmail.com>
2019-02-25 16:39:57 +0900

    Enable AWS API request and response logging in debug mode
    `github.com/aws/aws-sdk-go/aws/request.Waiter` doesn't provide useful logs,
    so we simply log AWS API request and response in debug mode.

--- myaws/config.go
+++ myaws/config.go
@@ -13,10 +13,22 @@ import (
 // profile, environment variables, IAM Task Role (ECS), IAM Role.
 // Unlike the aws default, load profile before environment variables
 // because we want to prioritize explicit arguments over the environment.
-func newConfig(profile string, region string) *aws.Config {
+func newConfig(profile string, region string, debug bool) *aws.Config {
 	defaultConfig := defaults.Get().Config
 	cred := newCredentials(getenv(profile, "AWS_DEFAULT_PROFILE"), getenv(region, "AWS_DEFAULT_REGION"))
-	return defaultConfig.WithCredentials(cred).WithRegion(getenv(region, "AWS_DEFAULT_REGION"))
+
+	logLevel := aws.LogLevel(aws.LogOff)
+	if debug {
+		// enable AWS API request and response logging in debug mode
+		logLevel = aws.LogLevel(aws.LogDebugWithHTTPBody | aws.LogDebugWithRequestRetries | aws.LogDebugWithRequestErrors)
+	}
+
+	config := defaultConfig.
+		WithCredentials(cred).
+		WithRegion(getenv(region, "AWS_DEFAULT_REGION")).
+		WithLogLevel(*logLevel)
+
+	return config
 }
 
 func newCredentials(profile string, region string) *credentials.Credentials {
