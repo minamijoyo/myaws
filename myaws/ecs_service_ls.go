e0ddd379ccfd5a0a14243c264e95c79dff43d671
Masayuki Morita <minamijoyo@gmail.com>
2019-02-28 18:58:49 +0900

    ecs/service/ls: Fix pagination issue
    We can specify up to 10 services to describe in a single operation.
    So we need to the list by 10.

--- myaws/ecs_service_ls.go
+++ myaws/ecs_service_ls.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/pkg/errors"
+	funk "github.com/thoas/go-funk"
 )
 
 // ECSServiceLsOptions customize the behavior of the Ls command.
@@ -15,31 +16,46 @@ type ECSServiceLsOptions struct {
 
 // ECSServiceLs describes ECS services.
 func (client *Client) ECSServiceLs(options ECSServiceLsOptions) error {
-	arns, err := client.ECS.ListServices(
+	serviceArns := []*string{}
+
+	err := client.ECS.ListServicesPages(
 		&ecs.ListServicesInput{
 			Cluster: &options.Cluster,
+		},
+		func(p *ecs.ListServicesOutput, lastPage bool) bool {
+			serviceArns = append(serviceArns, p.ServiceArns...)
+			return true
 		},
 	)
 	if err != nil {
 		return errors.Wrapf(err, "ListServices failed")
 	}
 
-	if len(arns.ServiceArns) == 0 {
+	if len(serviceArns) == 0 {
 		return errors.New("services not found")
 	}
 
-	services, err := client.ECS.DescribeServices(
-		&ecs.DescribeServicesInput{
-			Cluster:  &options.Cluster,
-			Services: arns.ServiceArns,
-		},
-	)
-
-	if len(services.Services) == 0 {
-		return errors.New("ListServices succeed, but DescribeServices returns no services")
+	// We can specify up to 10 services to describe in a single operation.
+	// So we need to divide the list by 10.
+	chunks := (funk.Chunk(serviceArns, 10)).([][]*string)
+	services := []*ecs.Service{}
+	for _, c := range chunks {
+		ss, err := client.ECS.DescribeServices(
+			&ecs.DescribeServicesInput{
+				Cluster:  &options.Cluster,
+				Services: c,
+			},
+		)
+		if err != nil {
+			return errors.Wrapf(err, "DescribeServices failed")
+		}
+		services = append(services, (ss.Services)...)
 	}
 
-	for _, service := range services.Services {
+	if len(services) == 0 {
+		return errors.New("ListServices succeed, but DescribeServices returns no services")
+	}
+	for _, service := range services {
 		fmt.Fprintln(client.stdout, formatECSService(client, options, service))
 	}
 
