41777a99171a768e68ce16b15f24b23b2d3084ad
Satoshi Tanaka <satoshi.tanaka@crowdworks.co.jp>
2020-01-23 11:26:38 +0900

    change AutoScalingSetInstanceProtectionInstances into private

--- myaws/autoscaling_set_instance_protection.go
+++ myaws/autoscaling_set_instance_protection.go
@@ -15,14 +15,14 @@ type AutoScalingSetInstanceProtectionOptions struct {
 // AutoScalingSetInstanceProtection protects from termination when scale in your autoscaling group.
 func (client *Client) AutoScalingSetInstanceProtection(options AutoScalingSetInstanceProtectionOptions) error {
 	if len(options.InstanceIds) > 0 {
-		if err := client.AutoScalingSetInstanceProtectionInstances(options.AsgName, options.InstanceIds, options.ProtectedFromScaleIn); err != nil {
+		if err := client.autoScalingSetInstanceProtectionInstances(options.AsgName, options.InstanceIds, options.ProtectedFromScaleIn); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (client *Client) AutoScalingSetInstanceProtectionInstances(asgName string, instanceIds []*string, protectedFromScaleIn bool) error {
+func (client *Client) autoScalingSetInstanceProtectionInstances(asgName string, instanceIds []*string, protectedFromScaleIn bool) error {
 	params := &autoscaling.SetInstanceProtectionInput{
 		AutoScalingGroupName: &asgName,
 		InstanceIds:          instanceIds,
